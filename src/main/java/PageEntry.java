dd138e0b1eee8f2ef29e60e9e24242b634ee4b37
Lenar Khafizov <lenarkh@mail.ru>
2023-02-12 10:15:47 +0300

    Инициализация выполнения дипломной работы с реализацией класса PageEntry.

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -3,5 +3,26 @@ public class PageEntry implements Comparable<PageEntry> {
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry (String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+    public String getPdfName() {
+        return pdfName;
+    }
+    public int getPage() {
+        return page;
+    }
+    public int getCount() {
+        return count;
+    }
+    @Override
+    public int compareTo(PageEntry pageEntry) {
+        return Integer.compare(pageEntry.getCount(), this.getCount());
+    }
+    @Override
+    public String toString() {
+        return "pdf=" + getPdfName() + ", page=" + getPage() + ", count=" + getCount();
+    }
 }
